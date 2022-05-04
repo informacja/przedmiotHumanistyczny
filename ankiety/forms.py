2760a48591a0fcd961c51efbfaa214224e2b2aa8
Piotr Wawryka <piotrwpwsz@gmail.com>
2022-05-04 20:36:19 +0200

    multiselect

--- ankiety/forms.py
+++ ankiety/forms.py
@@ -3,9 +3,15 @@ from django.core.exceptions import ValidationError
 from django.forms import models
 
 from ankiety.models import Odpowiedz
-
+COLORS= (
+    ('R', 'Red'),
+    ('B', 'Yellow'),
+    ('G', 'White'),
+)
 class Formularz(models.ModelForm):
     wiek = forms.IntegerField(min_value=18)
+    color = forms.MultipleChoiceField(choices=COLORS)
+
     # plec = forms.ChoiceField(choices=[
     #     ('k', 'Kobieta'),
     #     ('m', 'Mężczyzna'),
@@ -27,3 +33,14 @@ class Formularz(models.ModelForm):
         if wiek % 2:
             raise ValidationError('Wiek musi być parzysty')
         return wiek
+
+    def clean_color(self):
+        color = self.cleaned_data['color']
+        if not color:
+            raise forms.ValidationError("...")
+
+        if len(color) > 2:
+            raise forms.ValidationError("...")
+
+        color = ''.join(color)
+        return color
