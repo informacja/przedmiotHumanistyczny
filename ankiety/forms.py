c6da422ddf7fd183fcc0244fc8dedfb0202c2561
Piotr Wawryka <piotrwpwsz@gmail.com>
2022-05-05 23:57:12 +0200

    przkład

--- ankiety/forms.py
+++ ankiety/forms.py
@@ -1,41 +1,25 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.forms import models
-from django.db.models import Model
+
+# from django.db.models import Model
 from ankiety.models import Odpowiedz
+from django.forms import models
 
 class Formularz0(models.ModelForm):  # Przykładowy formularz
+
+    name = forms.CharField()
     wiek = forms.IntegerField(min_value=18)
-    #
-    # COLORS = (
-    #     ('R', 'Red'),
-    #     ('B', 'Yellow'),
-    #     ('G', 'White'),
-    # )
-    # color = forms.MultipleChoiceField(choices=COLORS)
     Jakie_jest_twoje_zaufanie_do_szczepien = forms.ChoiceField(choices=[
-        ('R',"lubię"),
-        ('2',"Nie ufam"),
+        (1,"lubię"),
+        (2,"Nie ufam"),
     ], widget=forms.RadioSelect)
 
-    # geeks_field = models.TextField()
-
-    Ile_masz_lat = forms.IntegerField(min_value=18)
     COLORS = (
         ('R', 'Red'),
         ('B', 'Yellow'),
         ('G', 'White'),
     )
     color = forms.MultipleChoiceField(choices=COLORS)
-
-    # Jakie_jest_toje_zaufanie_do_szczepień = forms.RadioSelect([
-    CHOICES = [
-        ('R',"lubię"),
-        ('2',"Nie ufam"),
-    ]
-
-    Jakie_jest_twoje_zaufanie_do_szczepien = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
-    # Jakie_jest_twoje_zaufanie_do_szczepień = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
 
     class Meta:
         model = Odpowiedz
--- ankiety/models.py
+++ ankiety/models.py
@@ -4,15 +4,16 @@ from django.db import models
 # from django.views.generic import ListView
 
 class Odpowiedz(models.Model):
+    Nowy_formularz_z_modelu = models.TextField()
     wiek = models.IntegerField()
 
+    # geeks_field = models.TextField()
     # wyksztalcenie = models.IntegerField(choices=[
     #     (1, 'Podstawowe'),
     #     (2, 'Średnie'),
     #     (3, 'Wyższe'),
     # ])
     #
-    # geeks_field = models.TextField()
 
      # gender = forms.ChoiceField(choices=[
     #     (1, 'Lubię'),
